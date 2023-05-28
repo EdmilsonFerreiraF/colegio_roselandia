1639a06e67cad94155964cef0735fe4014e6ddd3
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2023-05-28 09:46:12 -0300

    fix: novices modal asset and styling

--- components/novicesModal.tsx
+++ components/novicesModal.tsx
@@ -8,18 +8,91 @@ import * as React from "react";
 
 export default function NovicesModal() {
   const [open, setOpen] = React.useState(true);
-
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+  const [subscribedMessage, setSubscribedMessage] = React.useState(false);
 
   const handleClose = () => {
     setOpen(false);
   };
 
   const handleSubmit = () => {
-    handleClose();
+    setSubscribedMessage(true);
+    setTimeout(() => {
+      handleClose();
+    }, 3000);
   };
+
+  const subscribeModal = (
+    <Box component="form" className="form" onSubmit={handleSubmit} noValidate>
+      <Button className="closeButton" onClick={handleClose}>
+        x
+      </Button>
+      <div className="image">
+        <Image
+          src="open_enrollment_asset.jpg"
+          alt=""
+          height="300"
+          width="300"
+        />
+      </div>
+      <Box display="flex" flexDirection="column">
+        <Box display="flex" gap=".5rem">
+          <FormControl
+            sx={{
+              flex: 1,
+              marginBottom: ".35rem"
+            }}
+          >
+            <TextField
+              placeholder="Nome *"
+              sx={{
+                ".MuiInputBase-root": {
+                  height: "45px",
+                },
+              }}
+            />
+          </FormControl>
+          <FormControl
+            sx={{
+              flex: 1,
+            }}
+          >
+            <TextField
+              placeholder="Email *"
+              sx={{
+                ".MuiInputBase-root": {
+                  height: "45px",
+                },
+              }}
+            />
+          </FormControl>
+        </Box>
+        <FormControl
+          sx={{
+            flex: 1,
+          }}
+        >
+          <TextField
+            placeholder="+55"
+            sx={{
+              ".MuiInputBase-root": {
+                height: "45px",
+              },
+            }}
+          />
+        </FormControl>
+        <div className="description">
+          <p>Você pode atualizar seus dados a qualquer momento.</p>
+          <p>
+            Utilizaremos seus dados apenas para fins informativos, estamos
+            comprometidos em proteger e respeitar sua privacidade.
+          </p>
+        </div>
+      </Box>
+      <Button type="submit" className="submit">
+        Inscrever
+      </Button>
+    </Box>
+  );
 
   return (
     <div className="novicesModal">
@@ -35,85 +108,13 @@ export default function NovicesModal() {
           },
         }}
       >
-        {/* <DialogTitle>Subscribe</DialogTitle> */}
         <DialogContent className="novicesModal">
-          <Button className="closeButton" onClick={handleClose}>
-            x
-          </Button>
-          <div className="image">
-            <Image
-              src="open_enrollment_asset.jpg"
-              alt=""
-              height="300"
-              width="300"
-            />
-          </div>
-          <Box
-            component="form"
-            className="form"
-            display="flex"
-            flexDirection="column"
-          >
-            <Box display="flex" gap=".5rem">
-              <FormControl
-                sx={{
-                  flex: 1,
-                }}
-              >
-                <TextField
-                  placeholder="Nome *"
-                  sx={{
-                    ".MuiInputBase-root": {
-                      height: "45px",
-                    },
-                  }}
-                />
-              </FormControl>
-              <FormControl
-                sx={{
-                  flex: 1,
-                }}
-              >
-                <TextField
-                  placeholder="Email *"
-                  sx={{
-                    ".MuiInputBase-root": {
-                      height: "45px",
-                    },
-                  }}
-                />
-              </FormControl>
-            </Box>
-            <FormControl
-              sx={{
-                flex: 1,
-              }}
-            >
-              <TextField
-                placeholder="+55"
-                sx={{
-                  ".MuiInputBase-root": {
-                    height: "45px",
-                  },
-                }}
-              />
-            </FormControl>
-            <div className="description">
-              <p>Você pode atualizar seus dados a qualquer momento.</p>
-              <p>
-                Utilizaremos seus dados apenas para fins informativos, estamos
-                comprometidos em proteger e respeitar sua privacidade.
-              </p>
-            </div>
-          </Box>
-          <Button className="submit" onSubmit={handleSubmit}>
-            Inscrever
-          </Button>
+          {subscribedMessage ? (
+            <>Cadastrado com sucesso</>
+          ) : open ? (
+            subscribeModal
+          ) : null}
         </DialogContent>
-        {/* <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Subscribe</Button>
-        </DialogActions> */}
       </Dialog>
     </div>
   );
