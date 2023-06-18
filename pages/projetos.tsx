634f51f72292537c708cbd3732b038940fa70235
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2023-06-18 20:23:54 -0300

    refactor: change projects inside structure to show them from separated section instead of get them from a projects instance

--- pages/projetos.tsx
+++ pages/projetos.tsx
@@ -2,6 +2,7 @@ import Footer from "@/components/layout/footer/footer";
 import Header from "@/components/layout/header/header";
 import ProjectsInside from "@/components/section/inside/projectInside/projects";
 import OpenEnrollment from "@/components/section/openEnrollment";
+import { baseURL } from "@/constants/baseURL";
 import { AppContext } from "@/contexts/appProvider";
 import { Inter } from "next/font/google";
 import Image from "next/image";
@@ -12,45 +13,33 @@ const inter = Inter({ subsets: ["latin"] });
 const Projects = () => {
   const { isLoadingPages, pagesData } = useContext(AppContext) as any;
 
-  const [schoolAttributes, setSchoolAttributes] = useState([]);
   const [heroImages, setHeroImages] = useState([]);
-  const [educationLevel, setEducationLevel] = useState([]);
   const [projectsInside, setProjectsInside] = useState([]);
-  const [ourHistory, setOurHistory] = useState([]);
   const [openEnrollment, setOpenEnrollment] = useState([]);
-  const [partners, setPartners] = useState([]);
 
-  console.log("pagesData", pagesData);
-  useEffect(() => {
-    console.log("pagesData", pagesData);
-  }, [pagesData]);
-
-  const findPage = (page: "Home" | "projetos") => {
+  const findPage = (page: "Home" | "Projetos") => {
     const pageData = pagesData.find(
       (pageItem: any) => pageItem.titulo === page
     );
     return pageData;
   };
 
-  const homePage = findPage("Home");
-  const projectsPage = findPage("projetos");
+  const projectsPage = findPage("Projetos");
 
-  console.log("schoolAttributes", schoolAttributes);
   useEffect(() => {
-    console.log("isLoadingPages - index", isLoadingPages);
-    console.log("pages.data - index", pagesData.data);
     if (pagesData?.length) {
       setHeroImages(
-        homePage.blocos[0].item.carrossel.map((item: any) => item.item.imagem)
+        projectsPage.blocos[1].item.carrossel.map(
+          (item: any) => item.item.imagem
+        )
       );
       setProjectsInside(
         projectsPage.blocos[0].item.ensinos.map((item: any) => item.item)
       );
-      setOpenEnrollment(pagesData[0].blocos[3].item);
+      setOpenEnrollment(pagesData[0].blocos[2].item);
     }
   }, [pagesData, isLoadingPages]);
 
-  console.log("projectsInside", projectsInside);
   return (
     <main
       className={`projects-page projects flex min-h-screen flex-col items-center justify-between ${inter.className}`}
@@ -59,7 +48,12 @@ const Projects = () => {
         <Header />
         <div className="main">
           <div className="hero">
-            <Image width="100" height="100" src="hero.jpg" alt="" />
+            <Image
+              width="100"
+              height="100"
+              src={`${baseURL}/assets/${heroImages}`}
+              alt=""
+            />
           </div>
           <div className="title-subtitle">
             <div className="title">
